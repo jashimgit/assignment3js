3a1baac0d70c6e82bb48a54b5f65a49f8113df40
jashimgit <jashim.ncn@gmail.com>
2021-01-22 22:47:35 +0600

    kilometerToMeter,  budgetCalculator function and hotelCost function  added

---
+++ assignment.js
@@ -0,0 +1,110 @@
+// convert kilometer to meter
+
+function kilometerToMeter(kilometer) {
+
+    // check if parameter is negative
+    if (kilometer <= 0) {
+        var warning = "warning ! Number is either zero or negative";
+        return warning;
+    } else {
+        var result = kilometer * 1000;
+        return result;
+    }
+}
+
+
+
+// budget calculator
+
+function budgetCalculator(watch, phone, laptop) {
+
+    // Define product price 
+    var perWatchPrice = 50;
+    var perPhonePrice = 100;
+    var perLaptopPrice = 500;
+    var totalPrice = 0;
+
+    //  check if price is empty or negative 
+
+    if ((watch <= 0) || (watch == "")) {
+        // show warning
+        var warning = "Number of watch can not be negative";
+        return warning;
+    } else {
+        var totalWatchPrice = watch * perWatchPrice;
+        totalPrice += totalWatchPrice;
+
+    }
+
+    if ((phone <= 0) || (phone == "")) {
+        var warning = "Number of phone can not be negative";
+        return warning;
+    } else {
+        var totalPhonePrice = phone * perPhonePrice;
+        totalPrice += totalPhonePrice;
+
+    }
+
+    if ((laptop <= 0) || (laptop == "")) {
+        var warning = "Number of laptop can not be negative";
+        return warning;
+    } else {
+        var totalLaptopPrice = laptop * perLaptopPrice;
+        totalPrice += totalLaptopPrice;
+    }
+
+    return totalPrice;
+}
+
+
+// calculate hotelCost
+
+function hotelCost(days) {
+    var totalCost = 0;
+
+    // check if days is greater 10 or not 
+    if (days <= 10) {
+        if (days < 0) {
+            // show error if days is negative
+            console.log('number can not be zero or negative');
+        } else {
+            // calculate total days and total cost for number of days
+            var first10day = days;
+            first10daysCost = first10day * 100;
+            totalCost = first10daysCost;
+            return totalCost;
+        }
+
+    } else if ((days > 10) && (days <= 20)) {
+
+        if (days < 0) {
+            // show error if days is negative
+            console.log('number can not be zero ');
+        } else {
+            // calculate total days and total cost for number of days
+            var second10day = (days - 10);
+            var first10day = days - second10day;
+            var first10daysCost = first10day * 100;
+            var second10daysCost = second10day * 80;
+            totalCost = first10daysCost + second10daysCost;
+            return totalCost;
+        }
+
+    } else {
+        if (days < 0) {
+            // show error if days is negative
+            console.log('number can not be zero ');
+        } else {
+            // calculate total days and total cost for number of days
+            second10day = 10;
+            first10day = 10;
+            var restday = days - (first10day + second10day)
+            first10daysCost = first10day * 100;
+            second10daysCost = second10day * 80;
+            restDayCost = restday * 50;
+
+            totalCost = first10daysCost + second10daysCost + restDayCost;
+            return totalCost;
+        }
+    }
+}
